b8c3c6ff4620fe144407d014f95af372d876d76f
dsincennes <Donaldsincennes@hotmail.com>
2024-02-08 21:06:59 -0500

    Add project files.

---
+++ DesktopContactsApp/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System.Windows;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for ContactDetailsWindow.xaml
+    /// </summary>
+    public partial class ContactDetailsWindow : Window
+    {
+        Contact contact;
+        public ContactDetailsWindow(Contact contact)
+        {
+            InitializeComponent();
+
+            this.contact = contact;
+            nameTextBox.Text = contact.Name;
+            emailTextBox.Text = contact.Email;
+            phoneNumberTextBox.Text = contact.Phone;
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Delete(contact);
+            }
+
+            Close();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            contact.Name = nameTextBox.Text;
+            contact.Email = emailTextBox.Text;
+            contact.Phone = phoneNumberTextBox.Text;
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Update(contact);
+            }
+
+            Close();
+        }
+    }
+}
---
+++ DesktopContactsApp/NewContactWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System.Windows;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Contact contact = new Contact()
+            {
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneNumberTextBox.Text
+            };
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Insert(contact);
+            }
+
+            Close();
+        }
+    }
+}
